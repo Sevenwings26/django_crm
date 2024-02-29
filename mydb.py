0ebbb622486d00cc3afd7c3eda1443eb7ec98092
Iyanu Arowosola <114547715+Sevenwings26@users.noreply.github.com>
2024-02-29 04:16:09 +0100

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -23,13 +23,3 @@ print('Created database')
 
 
 
-# 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
-
-# git remote set-url origin https://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@github.com/Sevenwings26/django_crm_project
-
-
-# git credential approve << EOF
-# url=https://github.com
-# username=Sevenwings26
-# password= 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
-# EOF
